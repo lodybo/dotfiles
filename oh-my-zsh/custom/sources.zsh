6713c70b603511c2d21157e43554e429b430e80b
Lody Borgers <hi@lodybo.nl>
2021-11-02 16:56:14 +0100

    Re-ordered install script and added a sources file for zsh

---
+++ oh-my-zsh/custom/sources.zsh
@@ -0,0 +1,16 @@
+# This file sources everything I need into the Zsh environment.
+
+# Let's start with aliases
+source aliases/bat.zsh
+source aliases/exa.zsh
+source aliases/git.zsh
+source aliases/misc.zsh
+source aliases/npm.zsh
+
+# Then functions
+source functions/git.zsh
+source functions/zsh.zsh
+
+# Then autocompletions
+source autocompletions/bat.zsh
+source autocompletions/exa.zsh
