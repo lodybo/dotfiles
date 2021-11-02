b92e802cd8d2259c9bc3ddc0eee380b1070ff134
Lody Borgers <hi@lodybo.nl>
2021-11-02 19:13:56 +0100

    Fixed source paths and added script for loading nvm

--- oh-my-zsh/custom/sources.zsh
+++ oh-my-zsh/custom/sources.zsh
@@ -1,16 +1,16 @@
 # This file sources everything I need into the Zsh environment.
 
 # Let's start with aliases
-source aliases/bat.zsh
-source aliases/exa.zsh
-source aliases/git.zsh
-source aliases/misc.zsh
-source aliases/npm.zsh
+source $DOTFILES/oh-my-zsh/custom/aliases/bat.zsh
+source $DOTFILES/oh-my-zsh/custom/aliases/exa.zsh
+source $DOTFILES/oh-my-zsh/custom/aliases/git.zsh
+source $DOTFILES/oh-my-zsh/custom/aliases/misc.zsh
+source $DOTFILES/oh-my-zsh/custom/aliases/npm.zsh
 
 # Then functions
-source functions/git.zsh
-source functions/zsh.zsh
+source $DOTFILES/oh-my-zsh/custom/functions/git.zsh
+source $DOTFILES/oh-my-zsh/custom/functions/zsh.zsh
 
 # Then autocompletions
-source autocompletions/bat.zsh
-source autocompletions/exa.zsh
+source $DOTFILES/oh-my-zsh/custom/autocompletions/bat.zsh
+source $DOTFILES/oh-my-zsh/custom/autocompletions/exa.zsh
