59a13091195186523af0efa83fa11a777807bafd
Lody Borgers <l.borgers@taf.nl>
2021-11-05 15:20:02 +0100

    On macOS, Homebrew will be installed automatically

---
+++ macos/install-homebrew.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin";
+  then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+fi
+
+exit 0
