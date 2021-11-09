e1b8f1347b6c6598506c94057c276e0ee87d5cba
Lody Borgers <hi@lodybo.nl>
2021-11-09 20:28:19 +0100

    Created update aliases for macOS and Linux

---
+++ oh-my-zsh/linux/aliases-linux.zsh
@@ -0,0 +1,4 @@
+# Linux-only aliases
+
+# Update packages, update npm and node
+alias update='sudo apt update; sudo apt upgrade -y; nvm install --lts; npm install npm -g; npm update -g'
--- oh-my-zsh/macos/aliases-macos.zsh
+++ oh-my-zsh/macos/aliases-macos.zsh
@@ -2,3 +2,6 @@
 
 # Lock the screen (when going AFK)
 alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+# Get macOS Software Updates, and update installed Homebrew, node, npm, and their installed packages
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; nvm install --lts; npm install npm -g; npm update -g'
