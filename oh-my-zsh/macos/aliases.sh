5ca04a25378c205f44e18d1bbaf69e12b0e04ecc
Lody Borgers <l.borgers@taf.nl>
2022-11-23 12:13:11 +0100

    Added an alias to get the device's current IP address on a MacBook

--- oh-my-zsh/macos/aliases.sh
+++ oh-my-zsh/macos/aliases.sh
@@ -5,3 +5,6 @@ alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resource
 
 # Get macOS Software Updates, and update installed Homebrew, node, npm, and their installed packages
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; nvm install --lts; npm install npm -g; npm update -g'
+
+# Print this MacBook's IP address
+alias whatsmyip='ipconfig getifaddr en0'
