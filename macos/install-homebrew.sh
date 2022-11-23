bcbbc0c912bfb6ea73eee391072dd99ff57e4b60
Lody Borgers <l.borgers@taf.nl>
2022-11-23 17:13:28 +0100

    Finder does not show full dir paths in title anymore and Homebrew does a default packages install and is configured correctly for M1

--- macos/install-homebrew.sh
+++ macos/install-homebrew.sh
@@ -6,7 +6,7 @@
 # using Homebrew.
 
 # Check for Homebrew
-if test ! $(which brew)
+if test ! "$(which brew)"
 then
   echo "  Installing Homebrew for you."
 
@@ -17,5 +17,16 @@ then
   fi
 
 fi
+
+# Add Homebrew to the path for M1 MacBooks
+if  $(sysctl -n machdep.cpu.brand_string) =~ "M1"
+then
+  echo "Setting up Homebrew especially for a M1 MacBook.."
+  echo eval "$(/opt/homebrew/bin/brew shellenv)" >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# Install packages from a brewfile
+brew bundle --file "$DOTFILES/macos/Brewfile"
 
 exit 0
