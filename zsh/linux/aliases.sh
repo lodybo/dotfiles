0758c54fec911035d46c40b0c300bb0bb5a9a7cb
Lody Borgers <l.borgers@taf.nl>
2024-11-18 14:32:30 +0100

    Added alias for systemctl on linux

--- zsh/linux/aliases.sh
+++ zsh/linux/aliases.sh
@@ -1,4 +1,6 @@
 # Linux-only aliases
 
 # Update packages, update npm and node
-alias update='sudo apt update; sudo apt upgrade -y; nvm install --lts; npm install npm -g; npm update -g'
+alias update='sudo apt update; sudo apt upgrade -y;'
+
+alias ssctl='sudo systemctl'
